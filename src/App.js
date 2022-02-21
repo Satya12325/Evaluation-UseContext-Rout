936922d29a16cd0fc0b2e8e86e60f834b57c40b6
Satyabrata Das <satyabrata_pw1_135@masai.school>
2022-02-21 22:27:13 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import AllpageRout from './Router/AllpageRout';
-import Prerequsite from './Pages/Prerequsite';
+
 import Navbar from "./Pages/Navbar"
 function App() {
   return (
